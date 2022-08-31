f653691fa18027eb692003509b9f0247a62a09a2
João Victor <jvmelo1106@hotmail.com>
2022-08-31 17:58:14 -0300

    feat: criacao da home paciente

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { HomePaciente } from "./pages/HomePaciente";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePaciente />
+    </div>
+  );
 }
 
 export default App;
